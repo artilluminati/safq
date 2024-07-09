132c97a32a69cd51f585db65f7fcb2ef44b8cd5b
artilluminati <artilluminatigd@gmail.com>
2024-07-09 17:20:35 +0300

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// var options = {
+//     root: document.querySelector("#scrollArea"),
+//     rootMargin: "0px",
+//     threshold: 1.0,
+//   };
+//   var callback = function (entries, observer) {
+//     /* Content excerpted, show below */
+//   };
+//   var observer = new IntersectionObserver(callback, options);
